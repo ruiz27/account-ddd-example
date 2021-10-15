46d2a6ce27cf6dde37ac7a5fa4e47ebcaa1d0bc1
sruizvass <sergio.ruiz@vass.es>
2021-10-15 17:37:11 +0200

    Test para retiro de cuenta sin saldo

--- src/test/java/com/example/demo/domain/model/CuentaTest.java
+++ src/test/java/com/example/demo/domain/model/CuentaTest.java
@@ -10,19 +10,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class CuentaTest {
 
     @Test
-    public void whenSaldoEnCuentaEsIgualAlSaldoInicialThenSaldoOK(){
+    void whenSaldoEnCuentaEsIgualAlSaldoInicialThenSaldoOK(){
         Cuenta cuenta = new Cuenta(BigDecimal.ONE);
-        assertEquals(cuenta.saldo(),BigDecimal.ONE);
+        assertEquals(BigDecimal.ONE,cuenta.saldo());
     }
 
     @Test
-    public void givenCuentaWhenRetirarThenOK(){
+    void givenCuentaWhenRetirarThenOK(){
         // given
         Cuenta cuenta = new Cuenta(BigDecimal.valueOf(10));
         // when
         Cuenta newCuenta = cuenta.retirar(BigDecimal.valueOf(9));
         // then
         then(newCuenta.saldo()).isEqualTo(BigDecimal.ONE);
+    }
+
+    @Test
+    void givenCuentaWhenRetirarKO(){
+        // given
+        Cuenta cuenta = new Cuenta(BigDecimal.valueOf(1));
+        // when
+        Cuenta newCuenta = cuenta.retirar(BigDecimal.valueOf(9));
+        // then
+        then(newCuenta.saldo()).isEqualTo(BigDecimal.ONE);
+
     }
 
 
